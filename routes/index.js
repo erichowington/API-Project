761c07a7b014e12165389ac073401086f173803c
erichowington <ericehowington@gmail.com>
2024-03-05 12:36:11 -0500

    route documentation

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,16 @@ import planetsRoutes from "./planets.js"
 
 const router = Router()
 
-//Home page (http://localhost:3000/api)
+//         Home page (http://localhost:3000/api)
+
 router.get("/", (req,res) => res.send("This is the API root!"))
 
-//Routes that were imported to -(routes/planets.js)-
-//                   from -(controllers/planets.js)-
+
+//            Routes that were imported to
+//                -(routes/planets.js)-
+//                      from
+//              -(controllers/planets.js)-
+
 router.use( "/planets", planetsRoutes )
 
 export default router;
--- routes/planets.js
+++ routes/planets.js
@@ -3,19 +3,19 @@ import * as controllers from "../controllers/planets.js"
 
 const router = Router()
 
-//All Planets and their data.
+//         All Planets and their data.
 router.get("/", controllers.getPlanets);
 
-//One Planet and it's data.
+//          One Planet and it's data.
 router.get("/:id", controllers.getPlanet);
 
-//Create a new planet.
+//            Create a new planet.
 router.post("/", controllers.createPlanet);
 
-//Updata existing planet.
+//           Updata existing planet.
 router.put("/:id", controllers.updatePlanet);
 
-//Delete existing planet.
+//            Delete existing planet.
 router.delete("/:id", controllers.deletePlanet);
 
 
