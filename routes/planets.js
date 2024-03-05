14b6babf6f694d537595131dfdeb66e37c510bd9
erichowington <ericehowington@gmail.com>
2024-03-05 12:22:41 -0500

    route functionality

--- routes/planets.js
+++ routes/planets.js
@@ -3,10 +3,19 @@ import * as controllers from "../controllers/planets.js"
 
 const router = Router()
 
+//All Planets and their data.
 router.get("/", controllers.getPlanets);
+
+//One Planet and it's data.
 router.get("/:id", controllers.getPlanet);
+
+//Create a new planet.
 router.post("/", controllers.createPlanet);
+
+//Updata existing planet.
 router.put("/:id", controllers.updatePlanet);
+
+//Delete existing planet.
 router.delete("/:id", controllers.deletePlanet);
 
 
